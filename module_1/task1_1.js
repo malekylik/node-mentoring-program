7a38b19586269c1e3cc7219bee16f7b7f5cc3140
Maksim Kalinouski <maksim_kalinouoski@epam.com>
2020-07-27 18:54:18 +0300

    module_1/feat: implement task 1_1

---
+++ module_1/task1_1.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (input) => {
+    console.log(input.split('').reverse().join(''));
+});
+
+
