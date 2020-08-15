f3bd7c389da3034f7f1a16feee15692f58d94827
max <max@max.max>
2020-08-15 23:40:42 +0300

    module_2/feat: add base response

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-const a = '5';
+import express from 'express';
 
-console.log(a + 'asd');
+type User = {
+    id: string;
+    login: string;
+    password: string;
+    age: number;
+    isDeleted: boolean;
+};
+
+const app: express.Application = express();
+
+app.get('/:id', (req, res) => {
+    res.json(req.params);
+});
+
+app.listen(8080, () => {
+    console.log('Server started on port: 8080');
+});
