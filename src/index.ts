a3c76ffa0b541e628382e7e692f820cc893840e9
max <max@max.max>
2020-08-16 10:11:27 +0300

    module_2: add create and update methods

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 type User = {
     id: string;
@@ -8,12 +9,47 @@ type User = {
     isDeleted: boolean;
 };
 
+const savedUsers: Array<User> = [];
+
 const app: express.Application = express();
 
-app.get('/:id', (req, res) => {
-    res.json(req.params);
-});
+const router: express.Router = express.Router();
+
+app.use(express.json());
+
+router.route('/users')
+    .post((res, req) => {
+        const { login, password, age }: { login: string, password: string, age: string }
+            = (res.body as { login: string, password: string, age: string });
+
+        const user: User = {
+            login,
+            password,
+            id: uuidv4(),
+            age: Number(age),
+            isDeleted: false,
+        };
+
+        savedUsers.push(user);
+
+        req.status(201).json(user);
+    });
+
+router.route('/users/:id')
+    .get((req, res) => {
+        const { id }: { id: string } = (req.params as { id: string });
+
+        const user = savedUsers.find(user => user.id === id && !user.isDeleted);
+
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).end();
+        }
+    });
+
+app.use('/api/v1', router);
 
 app.listen(8080, () => {
-    console.log('Server started on port: 8080');
+    console.log('Server is running on port: 8080');
 });
