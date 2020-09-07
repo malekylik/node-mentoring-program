63f3fbae0adfe6d1cc0501bb88e1eccfb107ab4d
Maksim Kalinouski <maksim_kalinouoski@epam.com>
2020-09-07 21:31:50 +0300

    module_4/feat: add groups route

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import express from 'express';
 import { CreateUpdateUserValidation } from './schema';
 import { loadSequelize } from './loaders/sequelize.loader';
 import { loadUserModel } from './loaders/user-model.loader';
+import { loadGroupModel } from './loaders/group-model.loader';
 import { UserModel } from './models/user.model';
+import { GroupModel } from './models/group.model';
 import { UserService } from './services/user.service';
 import { getUsers, createUser, getUserById, updateUser, deleteUser, getAutoSuggestUsers } from './controllers/user.controller';
 
@@ -15,8 +17,11 @@ app.use(express.json());
 
 async function startApp() {
     const sequelize = await loadSequelize();
-    const userModalDB = loadUserModel(sequelize);
-    UserService.setUserModel(new UserModel(userModalDB));
+    const userModelDB = loadUserModel(sequelize);
+    const groupModelDB = loadGroupModel(sequelize);
+    UserService.setUserModel(new UserModel(userModelDB));
+
+    const groupModel = new GroupModel(groupModelDB);
 
     router.route('/users')
         .get(getUsers)
@@ -29,6 +34,20 @@ async function startApp() {
 
     router.route('/suggest')
         .get(getAutoSuggestUsers);
+
+    router.route('/groups')
+        .get(async (req, res) => {
+            const groups = await groupModel.getGroups();
+
+            res.json(groups);
+        })
+        .post(async (req, res) => {
+            const { name, permissions } = req.body;
+
+            const group = await groupModel.saveGroup({ name, permissions });
+
+            res.json(group);
+        });
 
     app.use('/api/v1', router);
 
