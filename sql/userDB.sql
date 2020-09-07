b1cb6141e28e9bffc1bc1d3413a4fc93c8a23aff
Maksim Kalinouski <maksim_kalinouoski@epam.com>
2020-09-07 21:29:22 +0300

    module_4/feat: add create Groups table to script

--- sql/userDB.sql
+++ sql/userDB.sql
@@ -10,6 +10,12 @@ CREATE TABLE Users(
     PRIMARY KEY (id)
 );
 
+CREATE TABLE Groups(
+    id SERIAL,
+    name VARCHAR (255) NOT NULL,
+    permissions VARCHAR (255) []
+);
+
 INSERT INTO Users (login, password, age) VALUES ('log2', 'pass2', 12);
 INSERT INTO Users (login, password, age) VALUES ('some-example-user-2', '5p4a3s2s', 55);
 INSERT INTO Users (login, password, age) VALUES ('js', 'node', 13);
