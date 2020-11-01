33b04e8200dd0a84787f3d18dcb57eab69c5cc84
Maksim Kalinouski <maksim_kalinouoski@epam.com>
2020-11-01 13:28:26 +0300

    module_7/feat: add test for getUsers

--- test/controllers/user.controller.spec.ts
+++ test/controllers/user.controller.spec.ts
@@ -1,7 +1,65 @@
-describe('some test', () => {
-    it('test', () => {
-        const a = 1;
+import 'regenerator-runtime/runtime';
 
-        expect(a).toBeTruthy();
+import express from 'express';
+
+import { getUsers } from 'app/controllers/user.controller';
+
+jest.mock('app/services/user.service', () => {
+    const mockUsers = [
+        {
+            id: '1',
+            login: 'mock_login_1',
+            password: 'mock_pass_1',
+            age: 12,
+            groups: [
+                {
+                    id: '1',
+                    name: 'group_1',
+                    permissions: ['READ', 'WRITE'],
+                },
+            ]
+        },
+        {
+            id: '2',
+            login: 'mock_login_2',
+            password: 'mock_pass_2',
+            age: 18,
+            groups: [
+                {
+                    id: '1',
+                    name: 'group_1',
+                    permissions: ['READ', 'WRITE'],
+                },
+                {
+                    id: '2',
+                    name: 'group_2',
+                    permissions: ['DELETE', 'SHARE'],
+                },
+            ]
+        },
+    ];
+
+    return {
+        mockUsers,
+        UserService: {
+            getUsers: jest.fn().mockResolvedValue(mockUsers),
+        },
+    };
+});
+
+describe('User controller', () => {
+    const mockUsers = jest.requireMock('../../src/services/user.service').mockUsers;
+
+    it('getUsers should return users', async () => {
+        const mockJson = jest.fn();
+
+        const mockReq = {};
+        const mockResp = {
+            json: mockJson,
+        }
+
+        await getUsers(mockReq as express.Request, mockResp as unknown as express.Response);
+
+        expect(mockJson).toBeCalledWith(mockUsers);
     });
 });
